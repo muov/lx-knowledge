b2a8c0fdd234711867692dfed32354629cfb15a4
小沐沐吖 <sunnyboy_mu@163.com>
2025-11-12 22:26:37 +0800

    feat: Web 笔记

---
+++ .vuepress/collections/frameworks-ecosystem.ts
@@ -0,0 +1,10 @@
+import { defineCollection } from "vuepress-theme-plume";
+
+export default defineCollection({
+  type: "doc",
+  dir: "2.🌐框架与生态",
+  title: "🌐框架与生态",
+  linkPrefix: "/frameworks-ecosystem",
+  sidebarCollapsed: true,
+  sidebar: "auto",
+});
--- src/🏗️ 框架与组件开发/README.md
+++
@@ -1,5 +0,0 @@
----
-title: 🏗️ 框架与组件开发
-createTime: 2025/11/01 15:27:35
-permalink: /components/
----
